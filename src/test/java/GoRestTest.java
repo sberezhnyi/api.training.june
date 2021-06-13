a01c77ad420b9849826452b9a698bf05fe835659
sberezhnyi <sergii.berezhnyi@globallogic.com>
2021-06-14 02:12:40 +0300

    3: Pure Java test example refactored

--- src/test/java/GoRestTest.java
+++ src/test/java/GoRestTest.java
@@ -1,20 +1,8 @@
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-
+import http.core.HttpRequestsBasicAuth;
 import lombok.extern.log4j.Log4j2;
-import lombok.extern.slf4j.Slf4j;
 
 import org.testng.annotations.Test;
 
-import static http.core.HeaderConstants.AUTHORIZATION;
-import static http.core.HeaderConstants.AUTHORIZATION_TYPE_BASIC;
 
 @Log4j2
 public class GoRestTest {
@@ -26,29 +14,12 @@ public class GoRestTest {
     private String password = "";
 
 
+    HttpRequestsBasicAuth httpRequestsBasicAuth = new HttpRequestsBasicAuth();
+
+
     @Test
     public void getUsers() {
 
-        String encoded = Base64.getEncoder()
-                .encodeToString((userName + ":" + password).getBytes(StandardCharsets.UTF_8));
-        Map<String, String> requestProperties = new HashMap<>();
-        requestProperties.put(AUTHORIZATION, AUTHORIZATION_TYPE_BASIC + encoded);
-
-        String result = "";
-        try {
-            URL url = new URL(host + apiPath + endPoint);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            requestProperties.forEach(conn::setRequestProperty);
-            InputStream output = conn.getInputStream();
-            String enc = conn.getHeaderField("Content-Encoding");
-            Scanner s = new Scanner(output).useDelimiter("\\A");
-            result = s.hasNext() ? s.next() : "";
-        } catch (IOException e) {
-
-            System.out.printf("Failed while working with url connection. Error msg: {%s}.", e.getMessage());
-        }
-
-        System.out.println(result);
-
+        log.info(httpRequestsBasicAuth.doGetRequest(host + apiPath + endPoint, userName, password));
     }
 }
