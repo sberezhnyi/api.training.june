4ca4978652c2a9d6dda9eb4931cd621c1ceae3cc
sberezhnyi <sergii.berezhnyi@globallogic.com>
2021-06-14 12:38:22 +0300

    19.2: Post with pojo

---
+++ src/main/java/util/User.java
@@ -0,0 +1,21 @@
+package util;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String gender;
+    private String status;
+    private String createdAt;
+    private String updatedAt;
+}
