4674ddadace91083200be2ac354363a782fd5587
sberezhnyi <sergii.berezhnyi@globallogic.com>
2021-06-14 12:05:00 +0300

    13: config examples

--- src/test/java/GoRestTest.java
+++ src/test/java/GoRestTest.java
@@ -1,8 +1,14 @@
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.GsonBuilder;
 import java.net.HttpURLConnection;
 import java.util.List;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.config.EncoderConfig;
+import io.restassured.config.HttpClientConfig;
+import io.restassured.config.ObjectMapperConfig;
 import io.restassured.http.ContentType;
+import io.restassured.parsing.Parser;
 import io.restassured.specification.RequestSpecification;
 import lombok.extern.log4j.Log4j2;
 
@@ -12,6 +18,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.apache.http.params.CoreConnectionPNames.CONNECTION_TIMEOUT;
 
 @Log4j2
 public class GoRestTest {
@@ -26,10 +33,31 @@ public class GoRestTest {
 
     private String id;
 
-    //    @BeforeClass
-    //    public void setUp(){
-    //        RestAssured.requestSpecification = setRequestSpec();
-    //    }
+    @BeforeClass
+    public void setUp(){
+        RestAssured.requestSpecification = setRequestSpec();
+        RestAssured.config = RestAssured.config()
+                .encoderConfig(
+                        EncoderConfig.encoderConfig()
+                                .appendDefaultContentCharsetToContentTypeIfUndefined(false)
+
+                )
+                .objectMapperConfig(
+                        new ObjectMapperConfig()
+                                //upper camel case for variable names in dto classes for GSON objects
+                                .gsonObjectMapperFactory((type, s) ->
+                                                                 new GsonBuilder()
+                                                                         .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                                                                         .create()
+
+                                )
+                );
+
+        //to extract body from response as json for assertion
+        RestAssured.registerParser("text/plain", Parser.JSON);
+        //log on failed tests
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
 
     private RequestSpecification setRequestSpec() {
 
@@ -51,12 +79,15 @@ public class GoRestTest {
     public void getUsers() {
 
         given()
+//                .spec(setRequestSpec())
+//                .spec(setRequestSpecExtraHeader())
                 .log().all()
-                .spec(setRequestSpec())
-                .spec(setRequestSpecExtraHeader())
+                .config(RestAssured.config()
+                                .httpClient(HttpClientConfig.httpClientConfig()
+                                                    .setParam(CONNECTION_TIMEOUT, 1000)))
                 .get(endPoint)
                 .then()
-                .statusCode(HttpURLConnection.HTTP_OK)
+                .statusCode(HttpURLConnection.HTTP_NOT_FOUND)
                 .extract()
                 .response()
                 .prettyPrint();
