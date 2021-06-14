ce17f20cf88fa545abcc6e3cb33e00b9a00206cc
sberezhnyi <sergii.berezhnyi@globallogic.com>
2021-06-14 10:43:12 +0300

    5: Headers

--- src/test/java/GoRestTest.java
+++ src/test/java/GoRestTest.java
@@ -1,4 +1,5 @@
 import java.net.HttpURLConnection;
+import io.restassured.http.ContentType;
 import lombok.extern.log4j.Log4j2;
 
 import org.testng.annotations.Test;
@@ -13,16 +14,22 @@ public class GoRestTest {
     private String endPoint = "/users";
     private String userName = "Howdy!";
     private String password = "";
+    private String token = "961e012678428d7ab28fb987546cb5a39a8d3445cacce0a38a93b3d5d6db40ff";
 
 
     @Test
     public void getUsers() {
 
         given()
+                .log()
+                .everything()
                 .baseUri(host)
                 .basePath(apiPath)
-                .auth()
-                .basic(userName, password)
+                .header("Authorization", "Bearer " + token)
+                .header("Connection", "keep-alive")
+                .header("Accept-Encoding", "gzip, deflate, br")
+                .contentType(ContentType.JSON)
+                .accept("*/*")
                 .get(endPoint)
                 .then()
                 .statusCode(HttpURLConnection.HTTP_OK)
